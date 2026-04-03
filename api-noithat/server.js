3bedf8683c808dd80c5d52dc8a1d6ff4fb469058
tamthien006 <ae7dff@gmail.com>
2026-04-03 21:07:18 +0700

    feat: tạo đơn hàng

--- api-noithat/server.js
+++ api-noithat/server.js
@@ -4,21 +4,21 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const danhMucRoutes = require('./src/routes/danhMuc.routes');
+// const danhMucRoutes = require('./src/routes/danhMuc.routes'); // TODO: tạo file
 const sanPhamRoutes = require('./src/routes/sanPham.routes');
 const taiKhoanRoutes = require('./src/routes/taiKhoan.routes');
 const gioHangRoutes = require('./src/routes/gioHang.routes');
 const donHangRoutes = require('./src/routes/donHang.routes');
-const thanhToanRoutes = require('./src/routes/thanhToan.routes');
-const chatRoutes = require('./src/routes/chat.routes');
-const danhGiaRoutes = require('./src/routes/danhGia.routes');
+// const thanhToanRoutes = require('./src/routes/thanhToan.routes'); // TODO: tạo file
+// const chatRoutes = require('./src/routes/chat.routes'); // TODO: tạo file
+// const danhGiaRoutes = require('./src/routes/danhGia.routes'); // TODO: tạo file
 const adminSanPhamRoutes = require('./src/routes/adminSanPham.routes');
-const adminDonHangRoutes = require('./src/routes/adminDonHang.routes');
-const adminHoaDonRoutes = require('./src/routes/adminHoaDon.routes');
+// const adminDonHangRoutes = require('./src/routes/adminDonHang.routes'); // TODO: tạo file
+// const adminHoaDonRoutes = require('./src/routes/adminHoaDon.routes'); // TODO: tạo file
 const adminDanhMucRoutes = require('./src/routes/adminDanhMuc.routes');
-const adminKhachHangRoutes = require('./src/routes/adminKhachHang.routes');
-const adminChatRoutes = require('./src/routes/adminChat.routes');
-const adminThongKeRoutes = require('./src/routes/adminThongKe.routes');
+// const adminKhachHangRoutes = require('./src/routes/adminKhachHang.routes'); // TODO: tạo file
+// const adminChatRoutes = require('./src/routes/adminChat.routes'); // TODO: tạo file
+// const adminThongKeRoutes = require('./src/routes/adminThongKe.routes'); // TODO: tạo file
 
 const app = express();
 
@@ -40,21 +40,21 @@ mongoose
   });
 
 // Routes
-app.use('/danh-muc', danhMucRoutes);
+// app.use('/danh-muc', danhMucRoutes);
 app.use('/san-pham', sanPhamRoutes);
 app.use('/tai-khoan', taiKhoanRoutes);
 app.use('/gio-hang', gioHangRoutes);
 app.use('/don-hang', donHangRoutes);
-app.use('/', thanhToanRoutes);
-app.use('/chat', chatRoutes);
-app.use('/', danhGiaRoutes);
+// app.use('/', thanhToanRoutes);
+// app.use('/chat', chatRoutes);
+// app.use('/', danhGiaRoutes);
 app.use('/', adminSanPhamRoutes);
-app.use('/', adminDonHangRoutes);
-app.use(adminHoaDonRoutes);
+// app.use('/', adminDonHangRoutes);
+// app.use(adminHoaDonRoutes);
 app.use(adminDanhMucRoutes);
-app.use('/', adminKhachHangRoutes);
-app.use('/', adminChatRoutes);
-app.use('/', adminThongKeRoutes);
+// app.use('/', adminKhachHangRoutes);
+// app.use('/', adminChatRoutes);
+// app.use('/', adminThongKeRoutes);
 
 // Health check
 app.get('/', (req, res) => {
--- api-noithat/src/models/DanhGia.js
+++ api-noithat/src/models/DanhGia.js
@@ -4,6 +4,7 @@ const DanhGiaSchema = new mongoose.Schema(
   {
     san_pham_id: { type: mongoose.Schema.Types.ObjectId, ref: 'SanPham', required: true },
     nguoi_dung_id: { type: mongoose.Schema.Types.ObjectId, ref: 'NguoiDung', required: true },
+    don_hang_id: { type: mongoose.Schema.Types.ObjectId, ref: 'DonHang' },
     so_sao: { type: Number, required: true, min: 1, max: 5 },
     noi_dung: { type: String, required: true, trim: true },
     ngay_tao: { type: Date, default: Date.now },
